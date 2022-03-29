92dfaf29e498f871684a7e5c3dd0511b76991051
Nelch3vez <88854087+Nelch3vez@users.noreply.github.com>
2022-03-29 14:52:29 -0600

    Update show.blade.php

---
+++ crudDeportes/resources/views/deportes/show.blade.php
@@ -0,0 +1,6 @@
+<label for="">Id -> {{$deporte->id}}</label>
+<label for="">Nombre -> {{$deporte->nombre}}</label>
+<label for="">Apellido -> {{$deporte->apellido}}</label>
+<label for="">Edad -> {{$deporte->edad}}</label>
+<label for="">Deporte -> {{$deporte->deporte}}</label>
+<label for="">Logro -> {{$deporte->logro}}</label>
